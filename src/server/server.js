4e5d9a7f3277d136f0acdb4fc8d30a7795a12144
Simone Marengo <Simone.Marengo@scai.intra>
2020-04-23 10:51:30 +0200

    wip, updated db connections and modified servers options

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,12 @@
-// server.js
-
 // set up ========================
 var express = require('express');
 var app = express();                                // create our app w/ express
 var mongoose = require('mongoose');                 // mongoose for mongodb
 var morgan = require('morgan');                     // log requests to the console (express4)
 var bodyParser = require('body-parser');            // pull information from HTML POST (express4)
-// var methodOverride = require('method-override');    // simulate DELETE and PUT (express4)
-var database = require('./config/database');
+var methodOverride = require('method-override');    // simulate DELETE and PUT (express4)
+var database = require('./config/database');        // import batabase configurations
 var port = process.env.PORT || 8888;                // set the port
-
 
 // Add headers
 app.all("/*", function (req, res, next) {
@@ -20,14 +17,28 @@ app.all("/*", function (req, res, next) {
 });
 
 // configuration ===============================================================
-mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
+let options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-// app.use(methodOverride());
+mongoose.connect(database.url, options).then(                  // connect to mongoDB database on modulus.io
+    res => {
+        console.log("database connected correctly");
+    },
+    err => {
+        console.log("DATABASE CONECTION ERROR!");
+    }
+);
+
+app.use(
+    express.static(__dirname + '/public'),                      // set the static files location /public/img will be /img for users
+    morgan('dev'),                                              // log every request to the console
+    bodyParser.urlencoded({ 'extended': 'true' }),              // parse application/x-www-form-urlencoded
+    bodyParser.json(),                                          // parse application/json
+    bodyParser.json({ type: 'application/vnd.api+json' }),      // parse application/vnd.api+json as json
+    methodOverride()
+);
 
 // routes ======================================================================
 require('./routes.js')(app);
